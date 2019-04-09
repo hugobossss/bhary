da75857fb08020ddb3600ea55c0f56725fbfaa35
benjaminwyk <wangyikai2333@gmail.com>
2019-04-09 11:28:28 +0200

    yikai 09042019

---
+++ snakewyk/src/Coordonnee.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ywang
+ */
+public class Coordonnee {
+    private double X;
+    private double Y;
+    
+    public Coordonnee(double x,double y) {
+        this.X = x;
+        this.Y = y;
+    }
+
+    public double getX() {
+        return X;
+    }
+
+    public double getY() {
+        return Y;
+    }
+    
+}
---
+++ snakewyk/src/projet/bhari/Trace.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ywang
+ */
+public class Trace {
+    
+    private String CouleurTrace;
+    private double[][] ListeCoordonnees [][];
+    
+}
